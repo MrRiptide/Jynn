31fd32744ff203873237a5fa647c3ebdd2f83c7a
MrRiptide <mrriptidemc@gmail.com>
2019-06-28 03:21:15 -0400

    SPACES

--- Jynn/modules/ascii.py
+++ Jynn/modules/ascii.py
@@ -7,96 +7,96 @@ import yaml
 
 
 class Art(object):
-	def __init__(self, art, tags, artist):
-		self.art = art
-		self.tags = tags
-		self.artist = artist
+    def __init__(self, art, tags, artist):
+        self.art = art
+        self.tags = tags
+        self.artist = artist
 
 
 def load_pending():
-	with open("ascii/pending.yaml", "r", encoding="utf8") as pending_file:
-		if pending_file.read().strip("\n") == "":
-			return []
-		print(pending_file)
-		pending = yaml.load(pending_file, Loader=yaml.FullLoader)
-		print(pending)
-		return pending
+    with open("ascii/pending.yaml", "r", encoding="utf8") as pending_file:
+        if pending_file.read().strip("\n") == "":
+            return []
+        print(pending_file)
+        pending = yaml.load(pending_file, Loader=yaml.FullLoader)
+        print(pending)
+        return pending
 
 
 def save_pending(pending):
-	pending_file = open("ascii/pending.yaml", "w")
-	yaml.dump(pending, pending_file)
-	pending_file.close()
+    pending_file = open("ascii/pending.yaml", "w")
+    yaml.dump(pending, pending_file)
+    pending_file.close()
 
 
 def load_library():
-	library_file = open("ascii/pending.yaml", "r")
-	if library_file.read().strip("\n") == "":
-		return []
-	library = yaml.load(library_file.read(), Loader=yaml.FullLoader)
-	library_file.close()
-	return library
+    library_file = open("ascii/pending.yaml", "r")
+    if library_file.read().strip("\n") == "":
+        return []
+    library = yaml.load(library_file.read(), Loader=yaml.FullLoader)
+    library_file.close()
+    return library
 
 
 def save_library(library):
-	library_file = open("ascii/pending.yaml", "w")
-	yaml.dump(library, library_file)
-	library_file.close()
+    library_file = open("ascii/pending.yaml", "w")
+    yaml.dump(library, library_file)
+    library_file.close()
 
 
 class ASCII(Cog):
-	"""
+    """
     ASCII module
 
     Includes ASCII library and image to ASCII
     """
 
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.group("ascii")
-	async def _ascii(self, ctx):
-		if ctx.invoked_subcommand is None:
-			await ctx.send("Invalid subcommand")
+    @commands.group("ascii")
+    async def _ascii(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("Invalid subcommand")
 
-	@_ascii.command(name="search")
-	async def _ascii_search(self, ctx, *tags):
-		pass
+    @_ascii.command(name="search")
+    async def _ascii_search(self, ctx, *tags):
+        pass
 
-	@_ascii.command(name="submit")
-	async def _ascii_submit(self, ctx):
-		last = await ctx.send("Enter ascii art:")
+    @_ascii.command(name="submit")
+    async def _ascii_submit(self, ctx):
+        last = await ctx.send("Enter ascii art:")
 
-		def check(m):
-			return m.author == ctx.author and m.channel == ctx.channel
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
 
-		try:
-			art_message = await self.bot.wait_for("message", check=check, timeout=30)
-			art = art_message.content
-		except asyncio.TimeoutError:
-			await last.delete()
-			timeout = await ctx.send("Timed out")
-			time.sleep(10)
-			await timeout.delete()
-			return
+        try:
+            art_message = await self.bot.wait_for("message", check=check, timeout=30)
+            art = art_message.content
+        except asyncio.TimeoutError:
+            await last.delete()
+            timeout = await ctx.send("Timed out")
+            time.sleep(10)
+            await timeout.delete()
+            return
 
-		await last.delete()
+        await last.delete()
 
-		last = await ctx.send("Enter tags separated by a comma")
+        last = await ctx.send("Enter tags separated by a comma")
 
-		try:
-			tags_message = await self.bot.wait_for("message", check=check, timeout=30)
-			tags = tags_message.content.replace(" ", "").split(",")
-		except asyncio.TimeoutError:
-			await last.delete()
-			timeout = await ctx.send("Timed out")
-			time.sleep(10)
-			await timeout.delete()
-			return
+        try:
+            tags_message = await self.bot.wait_for("message", check=check, timeout=30)
+            tags = tags_message.content.replace(" ", "").split(",")
+        except asyncio.TimeoutError:
+            await last.delete()
+            timeout = await ctx.send("Timed out")
+            time.sleep(10)
+            await timeout.delete()
+            return
 
-		await last.delete()
+        await last.delete()
 
-		confirmation = await ctx.send(f"""Are you sure you want to submit this ascii art?\n
+        confirmation = await ctx.send(f"""Are you sure you want to submit this ascii art?\n
         **Art:**
         {art_message.content}\n
         **Tags:**
@@ -104,90 +104,90 @@ class ASCII(Cog):
         React :white_check_mark: to confirm and :x: to cancel.
         """)
 
-		await confirmation.add_reaction("✅")
-		await confirmation.add_reaction("❌")
+        await confirmation.add_reaction("✅")
+        await confirmation.add_reaction("❌")
 
-		def check(emoji, user):
-			return user == ctx.author and str(emoji) in ["✅", "❌"]
+        def check(emoji, user):
+            return user == ctx.author and str(emoji) in ["✅", "❌"]
 
-		try:
-			reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
-		except asyncio.TimeoutError:
-			await confirmation.delete()
-			timeout = await ctx.send("Timed out")
-			time.sleep(10)
-			await timeout.delete()
-			return
-		if reaction.emoji == "✅":
-			ascii_art = Art(art, tags, ctx.author.id)
-			await confirmation.delete()
-			pending = load_pending()
-			pending.append(ascii_art)
-			save_pending(pending)
-			final = await ctx.send("Submitted for review")
-			time.sleep(10)
-			await final.delete()
-			return
-		if reaction.emoji == "❌":
-			await confirmation.delete()
-			cancelled = await ctx.send("Cancelled")
-			time.sleep(10)
-			await cancelled.delete()
-			return
+        try:
+            reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
+        except asyncio.TimeoutError:
+            await confirmation.delete()
+            timeout = await ctx.send("Timed out")
+            time.sleep(10)
+            await timeout.delete()
+            return
+        if reaction.emoji == "✅":
+            ascii_art = Art(art, tags, ctx.author.id)
+            await confirmation.delete()
+            pending = load_pending()
+            pending.append(ascii_art)
+            save_pending(pending)
+            final = await ctx.send("Submitted for review")
+            time.sleep(10)
+            await final.delete()
+            return
+        if reaction.emoji == "❌":
+            await confirmation.delete()
+            cancelled = await ctx.send("Cancelled")
+            time.sleep(10)
+            await cancelled.delete()
+            return
 
-	@_ascii.command(name="review")
-	async def _ascii_review(self, ctx):
-		pending = load_pending()
-		i = 0
-		msg = await ctx.send("Loading...")
-		while len(pending) > 0:
-			embed = discord.Embed(title=f"Pending art {i}/{len(pending)}")
-			item = pending[i]
-			embed.add_field(name="Art:", value=item.art)
-			embed.add_field(name="Tags:", value=", ".join(item.tags))
-			artist_user = self.bot.get_user(item.artist)
-			artist_name = f"{artist_user.display_name}#{artist_user.discriminator}"
-			embed.add_field(name="Artist:", value=artist_name)
-			await msg.edit("", embed=embed)
+    @_ascii.command(name="review")
+    async def _ascii_review(self, ctx):
+        pending = load_pending()
+        i = 0
+        msg = await ctx.send("Loading...")
+        while len(pending) > 0:
+            embed = discord.Embed(title=f"Pending art {i}/{len(pending)}")
+            item = pending[i]
+            embed.add_field(name="Art:", value=item.art)
+            embed.add_field(name="Tags:", value=", ".join(item.tags))
+            artist_user = self.bot.get_user(item.artist)
+            artist_name = f"{artist_user.display_name}#{artist_user.discriminator}"
+            embed.add_field(name="Artist:", value=artist_name)
+            await msg.edit("", embed=embed)
 
-			if i > 0:
-				await msg.add_reaction("⏪")
-			await msg.add_reaction("✅")
-			await msg.add_reaction("❌")
-			if i < len(pending) - 1:
-				await msg.add_reaction("⏩")
+            if i > 0:
+                await msg.add_reaction("⏪")
+            await msg.add_reaction("✅")
+            await msg.add_reaction("❌")
+            if i < len(pending) - 1:
+                await msg.add_reaction("⏩")
 
-			def check(emoji, user):
-				return user == ctx.author and str(emoji) in ["⏪", "✅", "🏳", "❌", "⏩"]
+            def check(emoji, user):
+                return user == ctx.author and str(emoji) in ["⏪", "✅", "🏳", "❌", "⏩"]
 
-			try:
-				reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
-			except asyncio.TimeoutError:
-				await msg.delete()
-				timeout = await ctx.send("Timed out")
-				time.sleep(10)
-				await timeout.delete()
-				return
-			if reaction.emoji == "⏪":
-				i -= 1
-			if reaction.emoji == "✅":
-				library = load_library()
-				library.append(item)
-				save_library(library)
-				pending.pop(i)
-				temp = await ctx.send("Accepted art")
-				time.sleep(5)
-				await temp.delete()
-			if reaction.emoji == "❌":
-				pending.pop(i)
-				temp = await ctx.send("Denied art")
-				time.sleep(5)
-				await temp.delete()
-			if reaction.emoji == "⏩":
-				i += 1
-		save_pending(pending)
-		await msg.edit(content="No more pending ascii art")
+            try:
+                reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
+            except asyncio.TimeoutError:
+                await msg.delete()
+                timeout = await ctx.send("Timed out")
+                time.sleep(10)
+                await timeout.delete()
+                return
+            if reaction.emoji == "⏪":
+                i -= 1
+            if reaction.emoji == "✅":
+                library = load_library()
+                library.append(item)
+                save_library(library)
+                pending.pop(i)
+                temp = await ctx.send("Accepted art")
+                time.sleep(5)
+                await temp.delete()
+            if reaction.emoji == "❌":
+                pending.pop(i)
+                temp = await ctx.send("Denied art")
+                time.sleep(5)
+                await temp.delete()
+            if reaction.emoji == "⏩":
+                i += 1
+        save_pending(pending)
+        await msg.edit(content="No more pending ascii art")
 
 
 def setup(bot):
-	bot.add_cog(ASCII(bot))
+    bot.add_cog(ASCII(bot))
--- Jynn/modules/formatting.py
+++ Jynn/modules/formatting.py
@@ -14,177 +14,177 @@ hollow = """`𝟙𝟚𝟛𝟜𝟝𝟞𝟟𝟠𝟡𝟘-=𝕢𝕨𝕖𝕣𝕥𝕪�
 box = """`1234567890-=🆀🆆🅴🆁🆃🆈🆄🅸🅾🅿[]\/🅰🆂🅳🅵🅶🅷🅹🅺🅻;'🆉🆇🅲🆅🅱🅽🅼,.~!@#$%^&*()_+🆀🆆🅴🆁🆃🆈🆄🅸🅾🅿{}|🅰🆂🅳🅵🅶🅷🅹🅺🅻:"🆉🆇🅲🆅🅱🅽🅼<>? """
 
 fonts = {
-	"normal": normal,
-	"king_james": king_james,
-	"cursive": cursive,
-	"hollow": hollow,
-	"box": box
+    "normal": normal,
+    "king_james": king_james,
+    "cursive": cursive,
+    "hollow": hollow,
+    "box": box
 }
 
 
 class Formatting(commands.Cog):
-	"""
-	Text formatting plugin
+    """
+    Text formatting plugin
 
-	Includes fonts and other formatting options
-	"""
+    Includes fonts and other formatting options
+    """
 
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.command(name="format")
-	async def _format(self, ctx, phrase, *formatting_options):
-		if "-upper" in formatting_options:
-			phrase = phrase.upper()
-		if "-lower" in formatting_options:
-			phrase = phrase.lower()
-		if "-font" in formatting_options:
-			font = get_argument(formatting_options, "-font", "cursive")
-			temp = ""
-			try:
-				for c in phrase:
-					temp += fonts[font][normal.index(c)]
-			except KeyError:
-				raise KeyError("Font chosen does not exist")
-			phrase = temp
-		if "-echo" in formatting_options:
-			try:
-				echos = int(get_argument(formatting_options, "-echo", 3))
-			except TypeError:
-				echos = 3
-			temp = ""
-			for c in phrase:
-				temp += c*echos
-			phrase = temp
-		if "-repeat" in formatting_options:
-			try:
-				repeats = int(get_argument(formatting_options, "-repeat", 2))
-			except TypeError:
-				repeats = 2
-			phrase = phrase*repeats
-		if "-spaces" in formatting_options:
-			try:
-				spaces = int(get_argument(formatting_options, "-spaces", 1))
-			except TypeError:
-				spaces = 1
-			temp = " " * spaces
-			phrase = temp.join(phrase)
-		if "-spoilereach" in formatting_options:
-			temp = ""
-			for c in phrase:
-				temp += f"||{c}||"
-			phrase = temp
-		if "-italics" in formatting_options:
-			phrase = f"*{phrase}*"
-		if "-bold" in formatting_options:
-			phrase = f"**{phrase}**"
-		if "-underline" in formatting_options:
-			phrase = f"__{phrase}__"
-		if "-spoiler" in formatting_options:
-			phrase = f"||{phrase}||"
-		await ctx.send(phrase)
-		await ctx.send(f"```{phrase}```")
+    @commands.command(name="format")
+    async def _format(self, ctx, phrase, *formatting_options):
+        if "-upper" in formatting_options:
+            phrase = phrase.upper()
+        if "-lower" in formatting_options:
+            phrase = phrase.lower()
+        if "-font" in formatting_options:
+            font = get_argument(formatting_options, "-font", "cursive")
+            temp = ""
+            try:
+                for c in phrase:
+                    temp += fonts[font][normal.index(c)]
+            except KeyError:
+                raise KeyError("Font chosen does not exist")
+            phrase = temp
+        if "-echo" in formatting_options:
+            try:
+                echos = int(get_argument(formatting_options, "-echo", 3))
+            except TypeError:
+                echos = 3
+            temp = ""
+            for c in phrase:
+                temp += c*echos
+            phrase = temp
+        if "-repeat" in formatting_options:
+            try:
+                repeats = int(get_argument(formatting_options, "-repeat", 2))
+            except TypeError:
+                repeats = 2
+            phrase = phrase*repeats
+        if "-spaces" in formatting_options:
+            try:
+                spaces = int(get_argument(formatting_options, "-spaces", 1))
+            except TypeError:
+                spaces = 1
+            temp = " " * spaces
+            phrase = temp.join(phrase)
+        if "-spoilereach" in formatting_options:
+            temp = ""
+            for c in phrase:
+                temp += f"||{c}||"
+            phrase = temp
+        if "-italics" in formatting_options:
+            phrase = f"*{phrase}*"
+        if "-bold" in formatting_options:
+            phrase = f"**{phrase}**"
+        if "-underline" in formatting_options:
+            phrase = f"__{phrase}__"
+        if "-spoiler" in formatting_options:
+            phrase = f"||{phrase}||"
+        await ctx.send(phrase)
+        await ctx.send(f"```{phrase}```")
 
-	@commands.command(name="font")
-	async def _font(self, ctx, phrase, font):
-		try:
-			new_phrase = pyfiglet.figlet_format(phrase, font)
-		except pyfiglet.FontNotFound:
-			return await ctx.send(f'The font `{font}` was not found.')
-		output = ""
-		for line in new_phrase.splitlines():
-			output += f"\u200B{line}\u200B\n"
-		if len(output) > 2000:
-			return await ctx.send('The text generated was too long for discord.')
-		return await ctx.send(f'```\n{output}```')
+    @commands.command(name="font")
+    async def _font(self, ctx, phrase, font):
+        try:
+            new_phrase = pyfiglet.figlet_format(phrase, font)
+        except pyfiglet.FontNotFound:
+            return await ctx.send(f'The font `{font}` was not found.')
+        output = ""
+        for line in new_phrase.splitlines():
+            output += f"\u200B{line}\u200B\n"
+        if len(output) > 2000:
+            return await ctx.send('The text generated was too long for discord.')
+        return await ctx.send(f'```\n{output}```')
 
-	@commands.command(name="fonts")
-	async def _fonts(self, ctx, page=1):
-		font_list = []
-		for file in os.listdir("../venv/Lib/site-packages/pyfiglet/fonts"):
-			if file.endswith(".flf"):
-				font = ""
-				for c in file.rstrip("flf").rstrip("."):
-					if not c.isalnum():
-						font += "\ ".rstrip(" ")
-					font += c
-				fonts.append(font)
+    @commands.command(name="fonts")
+    async def _fonts(self, ctx, page=1):
+        font_list = []
+        for file in os.listdir("../venv/Lib/site-packages/pyfiglet/fonts"):
+            if file.endswith(".flf"):
+                font = ""
+                for c in file.rstrip("flf").rstrip("."):
+                    if not c.isalnum():
+                        font += "\ ".rstrip(" ")
+                    font += c
+                fonts.append(font)
 
-		if page < 1 or page > math.ceil(len(fonts) / 20):
-			page = 1
+        if page < 1 or page > math.ceil(len(fonts) / 20):
+            page = 1
 
-		msg = await ctx.send(embed=gen_font_page(fonts, page))
-		await msg.add_reaction("⏮")
-		await msg.add_reaction("⏪")
-		await msg.add_reaction("⏩")
-		await msg.add_reaction("⏭")
+        msg = await ctx.send(embed=gen_font_page(fonts, page))
+        await msg.add_reaction("⏮")
+        await msg.add_reaction("⏪")
+        await msg.add_reaction("⏩")
+        await msg.add_reaction("⏭")
 
-		while True:
-			def check(emoji, user):
-				return user == ctx.author and str(emoji) in ["⏮", "⏪", "⏩", "⏭"]
+        while True:
+            def check(emoji, user):
+                return user == ctx.author and str(emoji) in ["⏮", "⏪", "⏩", "⏭"]
 
-			try:
-				reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
-			except asyncio.TimeoutError:
-				break
-			if reaction.emoji == "⏮":
-				page = 1
-			if reaction.emoji == "⏪" and page > 1:
-				page -= 1
-			if reaction.emoji == "⏩" and page < math.ceil(len(fonts) / 20):
-				page += 1
-			if reaction.emoji == "⏭":
-				page = math.ceil(len(fonts) / 20)
+            try:
+                reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
+            except asyncio.TimeoutError:
+                break
+            if reaction.emoji == "⏮":
+                page = 1
+            if reaction.emoji == "⏪" and page > 1:
+                page -= 1
+            if reaction.emoji == "⏩" and page < math.ceil(len(fonts) / 20):
+                page += 1
+            if reaction.emoji == "⏭":
+                page = math.ceil(len(fonts) / 20)
 
-			await reaction.remove(user)
+            await reaction.remove(user)
 
-			await msg.edit(embed=gen_font_page(fonts, page))
+            await msg.edit(embed=gen_font_page(fonts, page))
 
-	@commands.command(name="owoify", aliases=['owo', 'uwu'])
-	async def _owo(self, ctx, *, statement):
-		statement = replace_keep_case("l", "w", statement)
-		statement = replace_keep_case("r", "w", statement)
-		statement = replace_keep_case("ove", "uv", statement)
-		statement = replace_keep_case("na", "nya", statement)
-		statement = replace_keep_case("ne", "nye", statement)
-		statement = replace_keep_case("ni", "nyi", statement)
-		statement = replace_keep_case("no", "nyo", statement)
-		statement = replace_keep_case("nu", "nyu", statement)
-		await ctx.send(statement)
+    @commands.command(name="owoify", aliases=['owo', 'uwu'])
+    async def _owo(self, ctx, *, statement):
+        statement = replace_keep_case("l", "w", statement)
+        statement = replace_keep_case("r", "w", statement)
+        statement = replace_keep_case("ove", "uv", statement)
+        statement = replace_keep_case("na", "nya", statement)
+        statement = replace_keep_case("ne", "nye", statement)
+        statement = replace_keep_case("ni", "nyi", statement)
+        statement = replace_keep_case("no", "nyo", statement)
+        statement = replace_keep_case("nu", "nyu", statement)
+        await ctx.send(statement)
 
 
 def gen_font_page(fonts, page):
-	i = (page - 1) * 20
-	embed = discord.Embed(title=f"Page {page}/{math.ceil(len(fonts) / 20)}")
-	while i < page * 20 and i < len(fonts):
-		if i + 1 == len(fonts):
-			embed.add_field(name=fonts[i], value="\u200B", inline=False)
-		else:
-			embed.add_field(name=fonts[i], value=fonts[i + 1], inline=False)
-		i += 2
-	return embed
+    i = (page - 1) * 20
+    embed = discord.Embed(title=f"Page {page}/{math.ceil(len(fonts) / 20)}")
+    while i < page * 20 and i < len(fonts):
+        if i + 1 == len(fonts):
+            embed.add_field(name=fonts[i], value="\u200B", inline=False)
+        else:
+            embed.add_field(name=fonts[i], value=fonts[i + 1], inline=False)
+        i += 2
+    return embed
 
 def replace_keep_case(word, replacement, text):
-	def func(match):
-		g = match.group()
-		if g.islower():
-			return replacement.lower()
-		if g.istitle():
-			return replacement.title()
-		if g.isupper():
-			return replacement.upper()
-		return replacement
-	return re.sub(word, func, text, flags=re.I)
+    def func(match):
+        g = match.group()
+        if g.islower():
+            return replacement.lower()
+        if g.istitle():
+            return replacement.title()
+        if g.isupper():
+            return replacement.upper()
+        return replacement
+    return re.sub(word, func, text, flags=re.I)
 
 def get_argument(options, attribute, default=1):
-	try:
-		argument = options[options.index(attribute) + 1]
-	except IndexError:
-		return default
-	if argument.startswith("-"):
-		return default
-	return argument
+    try:
+        argument = options[options.index(attribute) + 1]
+    except IndexError:
+        return default
+    if argument.startswith("-"):
+        return default
+    return argument
 
 
 def setup(bot):
-	bot.add_cog(Formatting(bot))
+    bot.add_cog(Formatting(bot))
